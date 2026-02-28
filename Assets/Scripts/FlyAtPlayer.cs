94e90878486ee84b6ca2a1a6f3aa1a69f3f7e29f
Vefa Caglar Ozcan <vefacaglarozcan@gmail.com>
2026-02-28 18:51:49 +0300

    projectile must be at player's created pozition

--- Assets/Scripts/FlyAtPlayer.cs
+++ Assets/Scripts/FlyAtPlayer.cs
@@ -6,15 +6,21 @@ public class FlyAtPlayer : MonoBehaviour
     [SerializeField] private Transform player;
     [SerializeField] private float speed = 1f;
 
+    Vector3 playerPosition;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerPosition = player.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, playerPosition, speed * Time.deltaTime);
+        if (transform.position == playerPosition)
+        {
+            Destroy(gameObject);
+        }
     }
 }
